b59e013b749cea95b7f3213f12173d1050e185ea
Andrii Filipenko <andreyfil1337@gmail.com>
2025-04-15 19:39:18 +0300

    not working solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => {
+  console.log('jklj');
+
+  const { user, completed, title } = todo;
+
+  return (
+    <article className={`TodoInfo${completed && ' TodoInfo--completed'}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <a className="UserInfo" href={user.email}>
+        {user.title}
+      </a>
+    </article>
+  );
+};
